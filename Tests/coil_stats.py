8670174bc84c279d52de9f2ab3cae64c8e588e80
Bavykin Vlad <bavykinvlad752@gmail.com>
2024-07-05 14:31:11 +0300

    commit

--- Tests/coil_stats.py
+++ Tests/coil_stats.py
@@ -7,7 +7,7 @@ config = config_parser('../config.txt')
 
 base_url = f'http://{config['SERVER_HOST']}:{config['SERVER_PORT']}/api/coil/stats'
 
-params = {'date_start': '2024-05-20', 'date_end': '2024-05-21'}
+params = {'date_start': '2024-05-23', 'date_end': '2024-07-05'}
 
 query_string = urllib.parse.urlencode(params)
 
--- Tests/get.py
+++ Tests/get.py
@@ -3,7 +3,7 @@ from utils import config_parser
 
 config = config_parser('../config.txt')
 
-url = f'http://{config['SERVER_HOST']}:{config['SERVER_PORT']}/api/coil?id_min=3&id_max=5'
+url = f'http://{config['SERVER_HOST']}:{config['SERVER_PORT']}/api/coil?id_min=3&id_max=15'
 
 headers = {'Content-Type': 'application/json'}
 request = urllib.request.Request(url, headers=headers)
